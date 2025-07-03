95bedc03961592b3dc3bb8b6cc5bfd227c6efb8b
sheghurahmathullah <sheghu379@gmail.com>
2025-07-03 22:40:45 +0530

    pool update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,9 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+require("dotenv").config(); // load env vars if needed
+const { Pool } = require("pg");
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = pool;
