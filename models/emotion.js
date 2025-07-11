6cb04e4e24eff27e3ee5179bede8fc0ee75eb170
vembarasan-c <vembarasan624@gmail.com>
2025-07-11 15:41:38 +0530

    completed master data

--- models/emotion.js
+++ models/emotion.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
       code: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+        // unique: true,
       },
       score: {
         type: DataTypes.INTEGER,
